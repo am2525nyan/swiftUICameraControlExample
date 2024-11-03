580a4ab9c5d4997d0fc05030a417db463620f0c8
saki0411 <chipdale411@gmail.com>
2024-11-03 20:29:36 +0900

    Initial Commit

---
+++ AVCaptureEventInteractionSample/AVCaptureEventInteractionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVCaptureEventInteractionSampleApp.swift
+//  AVCaptureEventInteractionSample
+//
+//  Created by saki on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct AVCaptureEventInteractionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
